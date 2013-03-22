cd2aef59dde6fadfb8b12c182f0958ee53c5a67d
Denis Costa <deniscostadsc@gmail.com>
2013-03-21 22:04:58 -0300

    Extracting parsing methods to a new module and adding some tests.

--- assembla/models.py
+++ assembla/models.py
@@ -1,43 +1,18 @@
-from datetime import datetime
+import parsers
 
 
 class Model(object):
     @classmethod
     def parse(cls, json):
-        datetime_fields = [
-            'applied_at',
-            'begin_at',
-            'commercial_from',
-            'completed_date'
-            'created_at',
-            'created_on',
-            'due_date',
-            'end_at',
-            'filled_for',
-            'last_payer_changed_at',
-            'restricted_date',
-            'updated_at',
-        ]
-
-        entity = cls()
-        for key, value in json.items():
-
-            if key in datetime_fields:
-                if value:
-                    try:
-                        value = datetime.strptime(value[:19], '%Y-%m-%dT%H:%M:%S')
-                    except ValueError:
-                        value = datetime.strptime(value, '%Y-%m-%d').date()
-
-            setattr(entity, key, value)
-        return entity
+        attrs = parsers.parse(json)
+        instance = cls()
+        for key, value in attrs.items():
+            setattr(instance, key, value)
+        return instance
 
     @classmethod
     def parse_list(cls, json):
-        entities = []
-        for entity in json:
-            entities.append(cls.parse(entity))
-        return entities
+        return cls.parse_list(json)
 
 
 class User(Model):
---
+++ assembla/parsers.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+
+def parse(json):
+    datetime_fields = [
+        'applied_at',
+        'begin_at',
+        'commercial_from',
+        'completed_date',
+        'created_at',
+        'created_on',
+        'due_date',
+        'end_at',
+        'filled_for',
+        'last_payer_changed_at',
+        'restricted_date',
+        'updated_at',
+    ]
+
+    data = {}
+
+    for key, value in json.items():
+        if key in datetime_fields:
+            if value:
+                try:
+                    value = datetime.strptime(value[:19], '%Y-%m-%dT%H:%M:%S')
+                except ValueError:
+                    value = datetime.strptime(value, '%Y-%m-%d').date()
+        data[key] = value
+    return data
+
+
+def parse_list(json):
+    entities = []
+    for entity in json:
+        entities.append(entity)
+    return entities
