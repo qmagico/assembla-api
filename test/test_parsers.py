9f78f56f5ab51e5051dc59413669daf3f4fa4235
Denis Costa <deniscostadsc@gmail.com>
2013-08-03 14:21:52 -0300

    Improving tests

--- test/test_parsers.py
+++ test/test_parsers.py
@@ -11,19 +11,19 @@ from assembla import parsers
 
 class ParserTest(unittest.TestCase):
     def test_date_fields(self):
-        self.assertEquals(
+        self.assertEqual(
             {'restricted_date': datetime(2012, 1, 3, 0, 0).date()},
             parsers.parse({'restricted_date': '2012-01-03'})
         )
 
     def test_datetime_fields(self):
-        self.assertEquals(
+        self.assertEqual(
             {'created_on': datetime(2011, 9, 2, 10, 21, 48)},
             parsers.parse({'created_on': '2011-09-02T10:21:48Z'})
         )
 
     def test_float_fields(self):
-        self.assertEquals(
+        self.assertEqual(
             {'estimate': 6.5},
             parsers.parse({'estimate': '6.5'})
         )
